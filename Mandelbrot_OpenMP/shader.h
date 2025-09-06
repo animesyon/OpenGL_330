175265feded7a9322ae10962a8c07faf45b8923b
animesyon <121491211+animesyon@users.noreply.github.com>
2025-09-06 11:49:56 +0800

    initial commit

---
+++ Mandelbrot_OpenMP/shader.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+class CShader
+{
+private:
+    GLuint program;
+
+    void OutputShaderStatus(GLuint shader, GLenum pname, const char* name);
+    void OutputProgramStatus(GLuint program, GLenum pname);
+
+public:
+    CShader();
+    ~CShader();
+
+    bool Create(char* source1, char* source2);
+    void Destroy();
+    GLuint Get();
+
+    void Use();
+};
+
