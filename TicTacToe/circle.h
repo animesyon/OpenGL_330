1b6f316be6135e9a584abb989b685788b497a7a8
animesyon <121491211+animesyon@users.noreply.github.com>
2026-01-12 17:53:50 +0800

    initial commit

---
+++ TicTacToe/circle.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+class CCircle
+{
+private:
+	int location;
+	int count;
+	GLuint vao;
+
+	void Generate(int vertex_size, int* stride, float** vertices, int* size, int* count);
+
+public:
+	CCircle();
+	~CCircle();
+
+	void Create(GLuint handle);
+	void Destroy();
+	void Draw(float* matrix);
+};
