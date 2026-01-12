29b72d6e54b8323a11b996556d00da9e2327ccc7
animesyon <121491211+animesyon@users.noreply.github.com>
2026-01-12 17:57:35 +0800

    initial commit

---
+++ TicTacToe/grid.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+class CGrid
+{
+private:
+	int location1, location2;
+	int count;
+	GLuint vao;
+
+	void Generate(int vertex_size, int* stride, float** vertices, int* size, int* count);
+
+public:
+	CGrid();
+	~CGrid();
+
+	void Create(GLuint handle);
+	void Destroy();
+	void Draw(float* matrix);
+};
