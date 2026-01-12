dadf99f32520662cf1b4a444981f8ecfe2d2d024
animesyon <121491211+animesyon@users.noreply.github.com>
2026-01-12 18:03:26 +0800

    initial commit

---
+++ TicTacToe/shader.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+class CShader
+{
+private:
+    GLuint program;
+
+    void OutputShaderStatus(GLuint shader, GLenum pname, const char* name);
+    void OutputProgramStatus(GLuint program, GLenum pname);
+
+public:
+    CShader();
+    ~CShader();
+
+    bool Create(const char* source1, const char* source2);
+    void Destroy();
+    GLuint Get();
+
+    void Use();
+};
+
